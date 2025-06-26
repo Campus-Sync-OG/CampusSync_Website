84fedf4037e83661092559507818de94e2224232
SNayak07 <168377428+SNayak07@users.noreply.github.com>
2025-06-26 12:48:36 +0530

    added base url

--- src/api/ClientApi.js
+++ src/api/ClientApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://api-school-campus-og-b5c7anguhcdhgxbu.centralindia-01.azurewebsites.net/api",
   headers: {
     "Content-Type": "application/json",
   },
